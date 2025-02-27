c7b8c0539d1d3c99f58b5d218e866f88514b01f0
Hong La (Jason) <jason.la@thoughtworks.com>
2025-02-27 12:31:14 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:cart-typescript/src/cart.ts

--- cart-typescript/src/cart.ts
+++ cart-typescript/src/cart.ts
@@ -1,5 +1,13 @@
+import { Item } from './item';
+
 export class Cart {
+    private items: Item[] = [];
+
     isEmpty(): boolean {
-        return true;
+        return this.items.length === 0;
+    }
+
+    addItem(item: Item): void {
+        this.items.push(item);
     }
 }
